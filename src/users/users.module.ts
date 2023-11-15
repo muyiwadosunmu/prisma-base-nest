3ade988758dba785298c5b64e47041bf33c6811d
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2023-11-15 11:10:47 +0100

    Implemented control of data returning to client using Entity classes

---
+++ src/users/users.module.ts
@@ -0,0 +1,18 @@
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+@Module({
+  controllers: [UsersController],
+  providers: [
+    UsersService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
+  imports: [PrismaModule],
+})
+export class UsersModule {}
